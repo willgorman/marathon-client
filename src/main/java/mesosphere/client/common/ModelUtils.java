c13846086c7acd8a034d432ccce594fefec1af48
Michael Tweten <Michael.Tweten@Cerner.com>
2017-04-11 14:44:55 -0500

    Issue-147: Adds API for listing secret names.

--- src/main/java/mesosphere/client/common/ModelUtils.java
+++ src/main/java/mesosphere/client/common/ModelUtils.java
@@ -1,5 +1,6 @@
 package mesosphere.client.common;
 
+import mesosphere.dcos.client.model.ListSecretsResponse;
 import mesosphere.marathon.client.model.v2.Volume;
 
 import com.google.gson.Gson;
@@ -7,7 +8,7 @@ import com.google.gson.GsonBuilder;
 
 public class ModelUtils {
     public static final Gson GSON = new GsonBuilder().setPrettyPrinting()
-            .disableHtmlEscaping().registerTypeAdapter(Volume.class, new Volume.VolumeAdapter()).create();
+            .disableHtmlEscaping().registerTypeAdapter(Volume.class, new Volume.VolumeAdapter()).registerTypeAdapter(ListSecretsResponse.class, new ListSecretsResponse.ListSecretsResponseAdapter()).create();
 
     public static String toString(Object o) {
         return GSON.toJson(o);
